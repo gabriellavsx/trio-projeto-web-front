32df40199de8badd70e52d7e21c56285e1944c08
Maria Gabriella Victor <109552474+gabriellavsx@users.noreply.github.com>
2023-11-22 11:22:21 -0300

    cards

---
+++ scripts/NovaPesquisa.js
@@ -0,0 +1,26 @@
+
+function cadastrarPesquisa(){
+
+    var nomePesquisa = document.getElementById('txtNomePesquisa').value;
+    var dataPesquisa = document.getElementById('dtPesquisa').value;
+    var imagemPesquisa = document.getElementById('imgPesquisa').value;
+    
+    //criação do objeto
+    var novaPesquisa = {
+        nomePesquisa: nomePesquisa,
+        dataPesquisa: dataPesquisa,
+        imagemPesquisa: imagemPesquisa
+    };
+
+    // Recupera a lista de pesquisas do localStorage
+    var listaPesquisas = JSON.parse(localStorage.getItem('listaPesquisas')) || [];
+
+    // Adiciona a nova pesquisa à lista
+    listaPesquisas.push(novaPesquisa);
+    
+    // Armazena a lista atualizada no localStorage
+    localStorage.setItem('listaPesquisas', JSON.stringify(listaPesquisas));
+
+    window.location.href = 'Home.html';
+
+}
