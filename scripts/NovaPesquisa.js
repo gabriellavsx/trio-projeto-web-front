0efda8af177fbac7d5814861919f3de24f416a01
Maria Gabriella Victor <109552474+gabriellavsx@users.noreply.github.com>
2023-11-22 12:13:05 -0300

    s

--- scripts/NovaPesquisa.js
+++ scripts/NovaPesquisa.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 function cadastrarPesquisa(){
 
@@ -25,38 +24,6 @@ function cadastrarPesquisa(){
     window.location.href = 'Home.html';
 
 }
-=======
 function obterIdUsuarioLogado(){
     return localStorage.getItem('idUsuario') || null
 }
-
-function cadastrarPesquisa(){
-
-    var nomePesquisa = document.getElementById('txtNomePesquisa').value;
-    var dataPesquisa = document.getElementById('dtPesquisa').value;
-    var imagemPesquisa = document.getElementById('imgPesquisa').value;
-
-    var idUsuario = obterIdUsuarioLogado()
-
-    
-    //criação do objeto
-    var novaPesquisa = {
-        nomePesquisa: nomePesquisa,
-        dataPesquisa: dataPesquisa,
-        imagemPesquisa: imagemPesquisa,
-        idUsuario: idUsuario
-    };
-
-    // Recupera a lista de pesquisas do localStorage
-    var listaPesquisas = JSON.parse(localStorage.getItem('listaPesquisas')) || [];
-
-    // Adiciona a nova pesquisa à lista
-    listaPesquisas.push(novaPesquisa);
-    
-    // Armazena a lista atualizada no localStorage
-    localStorage.setItem('listaPesquisas', JSON.stringify(listaPesquisas));
-
-    window.location.href = 'Home.html';
-
-}
->>>>>>> 7c7c1da77946aa958d9e13a0e8ef573ace69c392
